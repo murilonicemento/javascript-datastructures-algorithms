96f8c70708a600314f76ff36b6e0479047d075a3
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-03 21:57:53 -0300

    testando depurador do vscode analisando a diferença de let e var

---
+++ text.js
@@ -0,0 +1,8 @@
+var framework = 'Angular';
+var framework = 'React';
+
+console.log(framework);
+
+let language = 'JavaScript!'; // {1}
+// let language = 'Ruby!'; // {2} - throws error
+console.log(language);
