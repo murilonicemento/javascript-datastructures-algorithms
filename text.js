db2f12fc0eb07acd598685324c2870817efa3c90
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-04 22:14:51 -0300

    delete file

--- text.js
+++
@@ -1,8 +0,0 @@
-var framework = 'Angular';
-var framework = 'React';
-
-console.log(framework);
-
-let language = 'JavaScript!'; // {1}
-// let language = 'Ruby!'; // {2} - throws error
-console.log(language);
