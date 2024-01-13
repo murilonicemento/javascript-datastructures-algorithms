921720adb3c07d0a0ca31a88f5a07bea7942a279
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-13 10:52:26 -0300

    criação função base converter

--- datastructures/stacks.js
+++ datastructures/stacks.js
@@ -30,6 +30,7 @@ class Stack {
 
   peek() {
     if (this.isEmpty()) return undefined;
+
     return this._items[this._count - 1];
   }
 
@@ -112,3 +113,32 @@ function decimalToBinary(decNumber) {
 console.log(decimalToBinary(233));
 console.log(decimalToBinary(10));
 console.log(decimalToBinary(1000));
+
+/****************************************************ALGORITMO CONVERSOR DE BASE************************************************** */
+
+function baseConverter(decNumber, base) {
+  const remStack = new Stack();
+  let number = decNumber;
+  let digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let rem;
+  let baseString = "";
+
+  if (!(base >= 2 && base <= 36)) return "";
+
+  while (number > 0) {
+    rem = Math.floor(number % base);
+    remStack.push(rem);
+    number = Math.floor(number / base);
+  }
+
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()];
+  }
+
+  return baseString;
+}
+
+console.log(baseConverter(100345, 2)); // 11000011111111001
+console.log(baseConverter(100345, 8)); // 303771
+console.log(baseConverter(100345, 16)); // 187F9
+console.log(baseConverter(100345, 35)); // 2BW0
