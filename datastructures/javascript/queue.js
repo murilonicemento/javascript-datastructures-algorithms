cada3823e8a51169a990a3590abc0c1857399ab2
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-13 15:27:06 -0300

    alteração de estruturas das pastas para melhor organização

--- datastructures/javascript/queue.js
+++ datastructures/javascript/queue.js
@@ -63,3 +63,19 @@ class Queue {
 }
 
 const queue = new Queue();
+
+console.log(queue.isEmpty());
+
+queue.enqueue("John");
+queue.enqueue("Jack");
+
+console.log(queue.toString());
+
+queue.enqueue("Camila");
+
+console.log(queue.toString());
+console.log(queue.size());
+console.log(queue.isEmpty());
+queue.dequeue();
+queue.dequeue();
+console.log(queue.toString());
