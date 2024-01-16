3b75967e273799cf54d4df4a8a501f247b6765f3
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-15 22:36:47 -0300

    usando a classe deque

--- datastructures/javascript/deque.js
+++ datastructures/javascript/deque.js
@@ -95,3 +95,30 @@ class Deque {
     return objString;
   }
 }
+
+const deque = new Deque();
+
+console.log(deque.isEmpty());
+
+deque.addBack("John");
+deque.addBack("Jack");
+
+console.log(deque.toString());
+
+deque.addBack("Camila");
+
+console.log(deque.toString());
+console.log(deque.size());
+console.log(deque.isEmpty());
+
+deque.removeFront();
+
+console.log(deque.toString());
+
+deque.removeBack();
+
+console.log(deque.toString());
+
+deque.addFront("John");
+
+console.log(deque.toString());
