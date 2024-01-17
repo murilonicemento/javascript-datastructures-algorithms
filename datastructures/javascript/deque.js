774b370035f84d57dba4cfdea43df63ef3d52ed3
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-16 22:31:41 -0300

    resolução exercício verificador de palíndromo

--- datastructures/javascript/deque.js
+++ datastructures/javascript/deque.js
@@ -122,3 +122,32 @@ console.log(deque.toString());
 deque.addFront("John");
 
 console.log(deque.toString());
+
+/** VERIFICADOR DE PALÍNDROMO */
+
+function palindromeChecker(aString) {
+  if (aString === undefined || aString === null || aString !== null && aString.length === 0) return false;
+
+  const deque = new Deque();
+  const lowerString = aString.toLocaleLowerCase().split(" ").join("");
+  let isEqual = true;
+  let firstChar, lastChar;
+
+  for (let i = 0; i < aString.length; i++) deque.addBack(aString[i]);
+
+  while (deque.size() > 1 && isEqual) {
+    firstChar = deque.removeFront();
+    lastChar = deque.removeBack();
+
+    if (firstChar !== lastChar) isEqual = false;
+  }
+
+  return isEqual;
+}
+
+console.log('a', palindromeChecker('a'));
+console.log('aa', palindromeChecker('aa'));
+console.log('kayak', palindromeChecker('kayak'));
+console.log('level', palindromeChecker('level'));
+console.log('Was it a car or a cat I saw', palindromeChecker('Was it a car or a cat I saw'));
+console.log('Step on no pets', palindromeChecker('Step on no pets'));
