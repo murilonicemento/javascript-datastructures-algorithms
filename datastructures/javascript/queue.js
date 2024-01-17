7755b860a98ea8eef6516d6b724b817e56574aba
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-16 22:17:50 -0300

    resolução exercício batata quente

--- datastructures/javascript/queue.js
+++ datastructures/javascript/queue.js
@@ -79,3 +79,36 @@ console.log(queue.isEmpty());
 queue.dequeue();
 queue.dequeue();
 console.log(queue.toString());
+
+/** FILA CIRCULAR - BATATA QUENTE */
+
+function hotPotato(elementList, num) {
+  const queue = new Queue;
+  const eliminatedList = [];
+
+  for (let i = 0; i < elementList.length; i++) {
+    queue.enqueue(elementList[i]);
+  }
+
+  while (queue.size() > 1) {
+    for (let i = 0; i < num; i++) {
+      queue.enqueue(queue.dequeue());
+    }
+
+    eliminatedList.push(queue.dequeue());
+  }
+
+  return {
+    "eliminated": eliminatedList,
+    "winner": queue.dequeue()
+  };
+}
+
+const names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl'];
+const result = hotPotato(names, 7);
+
+result.eliminated.forEach(name => {
+  console.log(`${name} was eliminated from the Hot Potato game.`);
+});
+
+console.log(`The winner is: ${result.winner}`);
