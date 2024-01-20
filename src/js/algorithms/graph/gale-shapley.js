b4ea690085d898024fdbe843a7f2940a231e9832
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-20 16:09:06 -0300

    criação método insert

--- src/js/algorithms/graph/gale-shapley.js
+++ src/js/algorithms/graph/gale-shapley.js
@@ -1,77 +1,77 @@
 class Person {
-	constructor(id, gender, ranks, rankIndex, status = "single") {
-		this.id = id
-		this.gender = gender
-		this.ranks = ranks
-		this.i = rankIndex
-		this.status = status
-		this.partner = null
-	}
+  constructor(id, gender, ranks, rankIndex, status = 'single') {
+    this.id = id;
+    this.gender = gender;
+    this.ranks = ranks;
+    this.i = rankIndex;
+    this.status = status;
+    this.partner = null;
+  }
 }
 
-function galeShapley(proposers, reviewers) {
-	let perfectMatching = new Map()
-	for (let [pid, p] of proposers) {
-		while (p.i < p.ranks.length) {
-			let rid = p.ranks[p.i]
-			let r = reviewers.get(rid)
-			p.i++
-			if (r.status === "single") {
-				r.status = "engaged"
-				r.partner = p
-				p.partner = r
-				perfectMatching.set(pid, rid)
-				proposers.delete(pid)
-				break
-			} else if (r.status === "engaged" && r.ranks.get(pid) < r.ranks.get(r.partner.id)) {
-				perfectMatching.delete(r.partner.id)
-				proposers.set(r.partner.id, r.partner)
-				proposers.delete(pid)
-				r.partner.partner = null
-				r.partner = p
-				p.partner = r
-				perfectMatching.set(pid, rid)
-				break
-			}
-		}
-	}
-	return perfectMatching
-}
+// function galeShapley(proposers, reviewers) {
+//   const perfectMatching = new Map();
+//   for (const [pid, p] of proposers) {
+//     while (p.i < p.ranks.length) {
+//       const rid = p.ranks[p.i];
+//       const r = reviewers.get(rid);
+//       p.i++;
+//       if (r.status === 'single') {
+//         r.status = 'engaged';
+//         r.partner = p;
+//         p.partner = r;
+//         perfectMatching.set(pid, rid);
+//         proposers.delete(pid);
+//         break;
+//       } else if (r.status === 'engaged' && r.ranks.get(pid) < r.ranks.get(r.partner.id)) {
+//         perfectMatching.delete(r.partner.id);
+//         proposers.set(r.partner.id, r.partner);
+//         proposers.delete(pid);
+//         r.partner.partner = null;
+//         r.partner = p;
+//         p.partner = r;
+//         perfectMatching.set(pid, rid);
+//         break;
+//       }
+//     }
+//   }
+//   return perfectMatching;
+// }
 
-let men = new Map()
-let women = new Map()
-//O(n^2)
-function createPeople(n, gender, set) {
-	for (let i = 0; i < n; i++) {
-		let id = i
-		let rankIndex = 0
-		let g = gender
-		let ranks = new Array(n)
-		let p
-		for (let i = 0; i < n; i++) ranks[i] = i
-		shuffle(ranks)
-		if (gender == 'w') {
-			let wRanks = new Map()
-			for (let i = 0; i < n; i++) wRanks.set(i, ranks[i])
-			p = new Person(id, g, wRanks, null)
-		} else {
-			p = new Person(id, g, ranks, rankIndex)
-		}
-		set.set(i, p)
-	}
-}
-
-//fisher-yates shuffle O(n)
+// fisher-yates shuffle O(n)
 function shuffle(array) {
-	for (let i = array.length - 1; i >= 0; i--) {
-		let j = Math.floor(Math.random() * (i + 1))
-		let temp = array[i]
-		array[i] = array[j]
-		array[j] = temp
-	}
+  for (let i = array.length - 1; i >= 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
 }
 
-createPeople(4, 'm', men)
-createPeople(4, 'w', women)
-let stableMatching = galeShapley(men, women)
-console.log(stableMatching)
+const men = new Map();
+const women = new Map();
+// O(n^2)
+function createPeople(n, gender, set) {
+  for (let i = 0; i < n; i++) {
+    const id = i;
+    const rankIndex = 0;
+    const g = gender;
+    const ranks = new Array(n);
+    let p;
+    for (let j = 0; j < n; j++) ranks[j] = j;
+    shuffle(ranks);
+    if (gender === 'w') {
+      const wRanks = new Map();
+      for (let k = 0; k < n; k++) wRanks.set(k, ranks[k]);
+      p = new Person(id, g, wRanks, null);
+    } else {
+      p = new Person(id, g, ranks, rankIndex);
+    }
+    set.set(i, p);
+  }
+}
+
+createPeople(4, 'm', men);
+createPeople(4, 'w', women);
+// const stableMatching = galeShapley(men, women);
+// console.log(stableMatching);
--- src/js/data-structures/doubly-linked-list.js
+++ src/js/data-structures/doubly-linked-list.js
@@ -1,25 +1,142 @@
-import { defaultEquals } from '../util';
-import LinkedList from './linked-list';
-import { DoublyNode } from './models/linked-list-models';
+// import { defaultEquals } from '../util';
+// import LinkedList from './linked-list';
+// import { DoublyNode } from './models/linked-list-models';
 
-export default class DoublyLinkedList extends LinkedList {
+function defaultEquals(a, b) {
+  return a === b;
+}
+
+class LinkedList {
   constructor(equalsFn = defaultEquals) {
-    super(equalsFn);
-    this.tail = undefined;
+    this.equalsFn = equalsFn;
+    this.count = 0;
+    this.head = undefined;
   }
 
   push(element) {
-    const node = new DoublyNode(element);
-    if (this.head == null) {
+    const node = new Node(element); // {1}
+    let current; // {2}
+    if (this.head == null) { // {3}
       this.head = node;
-      this.tail = node; // NEW
     } else {
-      // attach to the tail node // NEW
-      this.tail.next = node;
-      node.prev = this.tail;
-      this.tail = node;
+      current = this.head; // {4}
+      while (current.next != null) { // {5} obtém o último item
+        current = current.next;
+      }
+      // e atribui o novo elemento a next para criar a ligação
+      current.next = node; // {6}
     }
-    this.count++;
+    this.count++; // {7}
+  }
+
+  removeAt(index) {
+    // verifica valores fora do intervalo
+    if (index >= 0 && index < this.count) { // {1}
+      let current = this.head; // {2}
+      // remove o primeiro item
+      if (index === 0) { // {3}
+        this.head = current.next;
+      } else {
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        // let previous; // {4}
+        // for (let i = 0; i < index; i++) { // {5}
+        //   previous = current; // {6}
+        //   current = current.next; // {7}
+        // }
+        // faz a ligação de previous com o next de current: pula esse elemento para removê-lo
+        previous.next = current.next; // {8}
+      }
+      this.count--; // {9}
+      return current.element;
+    }
+    return undefined; // {10}
+  }
+
+  getElementAt(index) {
+    if (index >= 0 && index <= this.count) { // {1}
+      let node = this.head; // {2}
+
+      for (let i = 0; i < index && node != null; i++) { // {3}
+        node = node.next;
+      }
+      return node; // {4}
+    }
+    return undefined; // {5}
+  }
+
+  insert(element, index) {
+    if (index >= 0 && index <= this.count) { // {1}
+      const node = new Node(element);
+      if (index === 0) { // adiciona na primeira posição
+        const current = this.head;
+        node.next = current; // {2}
+        this.head = node;
+      } else {
+        const previous = this.getElementAt(index - 1); // {3}
+        const current = previous.next; // {4}
+        node.next = current; // {5}
+        previous.next = node; // {6}
+      }
+      this.count++; // atualiza o tamanho da lista
+      return true;
+    }
+    return false; // {7}
+  }
+
+  indexOf(element) {
+    let current = this.head; // {1}
+    for (let i = 0; i < this.count && this.count != null; i++) { // {2}
+      if (this.equalsFn(element, current.element)) { // {3}
+        return i; // {4}
+      }
+      current = current.next; // {5}
+    }
+    return -1; // {6}
+  }
+
+  remove(element) {
+    const index = this.indexOf(element);
+    return this.removeAt(index);
+  }
+
+  size() {
+    return this.count;
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  toString() {
+    if (this.head == null) return ''; // {1}
+
+    let objString = `${this.head.element}`; // {2}
+    let current = this.head.next; // {3}
+
+    for (let i = 1; i < this.size() && current != null; i++) { // {4}
+      objString = `${objString}, ${current.element}`;
+      current = current.next;
+    }
+
+    return objString; // {5}
+  }
+}
+
+class DoublyNode extends Node {
+  constructor(element, next, prev) {
+    super(element, next);
+    this.prev = prev; // NOVO
+  }
+}
+class DoublyLinkedList extends LinkedList {
+  constructor(equalsFn = defaultEquals) {
+    super(equalsFn);
+    this.tail = undefined; // NOVO
   }
 
   insert(element, index) {
@@ -27,112 +144,33 @@ export default class DoublyLinkedList extends LinkedList {
       const node = new DoublyNode(element);
       let current = this.head;
       if (index === 0) {
-        if (this.head == null) { // NEW
+        if (this.head == null) { // {1} NOVO
           this.head = node;
-          this.tail = node; // NEW
+          this.tail = node;
         } else {
-          node.next = this.head;
-          this.head.prev = node; // NEW
-          this.head = node;
+          node.next = this.head; // {2}
+          current.prev = node; // {3} NOVO
+          this.head = node; // {4}
         }
-      } else if (index === this.count) { // last item NEW
-        current = this.tail;
-        current.next = node;
-        node.prev = current;
-        this.tail = node;
+      } else if (index === this.count) { // último item – NOVO
+        current = this.tail; // {5}
+        current.next = node; // {6}
+        node.prev = current; // {7}
+        this.tail = node; // {8}
       } else {
-        const previous = this.getElementAt(index - 1);
-        current = previous.next;
-        node.next = current;
-        previous.next = node;
-        current.prev = node; // NEW
-        node.prev = previous; // NEW
+        const previous = this.getElementAt(index - 1); // {9}
+        current = previous.next; // {10}
+        node.next = current; // {11}
+        previous.next = node; // {12}
+        current.prev = node; // {13}
+        node.prev = previous; // {14}
       }
       this.count++;
       return true;
     }
     return false;
   }
-
-  removeAt(index) {
-    if (index >= 0 && index < this.count) {
-      let current = this.head;
-      if (index === 0) {
-        this.head = this.head.next;
-        // if there is only one item, then we update tail as well //NEW
-        if (this.count === 1) {
-          // {2}
-          this.tail = undefined;
-        } else {
-          this.head.prev = undefined;
-        }
-      } else if (index === this.count - 1) {
-        // last item //NEW
-        current = this.tail;
-        this.tail = current.prev;
-        this.tail.next = undefined;
-      } else {
-        current = this.getElementAt(index);
-        const previous = current.prev;
-        // link previous with current's next - skip it to remove
-        previous.next = current.next;
-        current.next.prev = previous; // NEW
-      }
-      this.count--;
-      return current.element;
-    }
-    return undefined;
-  }
-
-  indexOf(element) {
-    let current = this.head;
-    let index = 0;
-    while (current != null) {
-      if (this.equalsFn(element, current.element)) {
-        return index;
-      }
-      index++;
-      current = current.next;
-    }
-    return -1;
-  }
-
-  getHead() {
-    return this.head;
-  }
-
-  getTail() {
-    return this.tail;
-  }
-
-  clear() {
-    super.clear();
-    this.tail = undefined;
-  }
-
-  toString() {
-    if (this.head == null) {
-      return '';
-    }
-    let objString = `${this.head.element}`;
-    let current = this.head.next;
-    while (current != null) {
-      objString = `${objString},${current.element}`;
-      current = current.next;
-    }
-    return objString;
-  }
-
-  inverseToString() {
-    if (this.tail == null) {
-      return '';
-    }
-    let objString = `${this.tail.element}`;
-    let previous = this.tail.prev;
-    while (previous != null) {
-      objString = `${objString},${previous.element}`;
-      previous = previous.prev;
-    }
-    return objString;
-  }
 }
+
+const doublyLinkedList = new DoublyLinkedList();
+console.log(doublyLinkedList);
