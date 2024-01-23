52eded0ddc6df78f01f9e608a6aed66e09a61753
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-23 19:56:27 -0300

    criação método removeAt

--- src/js/data-structures/circular-linked-list.js
+++ src/js/data-structures/circular-linked-list.js
@@ -165,6 +165,32 @@ class CircularLinkedList extends LinkedList {
 
     return false;
   }
+
+  removeAt(index) {
+    if (index >= 0 && index < this.count) {
+      let current = this.head;
+      if (index === 0) {
+        if (this.size() === 1) {
+          this.head = undefined;
+        } else {
+          const removed = this.head;
+          current = this.getElementAt(this.size());
+          this.head = this.head.next;
+          current.next = this.head;
+          current = removed;
+        }
+      } else {
+        // não há necessidade de atualizar o último elemento da lista circular
+        const previous = this.getElementAt(index - 1);
+        current = previous.next;
+        previous.next = current.next;
+      }
+      this.count--;
+      return current.element;
+    }
+
+    return undefined;
+  }
 }
 
 const circularLinkedList = new CircularLinkedList();
