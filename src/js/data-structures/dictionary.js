710cdf7b1787bbce416b0814b2461e44162b297c
murilonicemento <murilo_nascimento22@outlook.com>
2024-01-27 11:13:38 -0300

    criação métodos hasKey

--- src/js/data-structures/dictionary.js
+++ src/js/data-structures/dictionary.js
@@ -1,81 +1,61 @@
-import { defaultToString } from '../util';
-import { ValuePair } from './models/value-pair';
+// import { defaultToString } from '../util';
+// import { ValuePair } from './models/value-pair';
 
-export default class Dictionary {
+/**
+ * • set(key, value): esse método adiciona um novo elemento ao dicionário.
+  Se key já existir, seu valor será sobrescrito com o novo valor.
+ * • remove(key): esse método remove value do dicionário usando key como o parâmetro de busca.
+ * • hasKey(key): esse método devolve true se key estiver presente no dicionário,
+  e false caso contrário.
+ * • get(key): esse método devolve um value específico do dicionário usando
+ key como o parâmetro de busca.
+ * • clear(): esse método remove todos os valores do dicionário.
+ * • size(): esse método devolve a quantidade de valores contida no dicionário.
+  É semelhante à propriedade length da classe Array.
+ * • isEmpty(): esse método devolve true se size for igual a zero, e false caso contrário.
+ * • keys(): esse método devolve um array com todas as chaves contidas no dicionário.
+ * • values(): esse método devolve um array com todos os valores contidos no dicionário.
+ * • keyValues(): esse método devolve um array com todos os pares de valores
+  [chave, valor] contidos no dicionário.
+ * • forEach(callBackFn): esse método itera pelos valores (value) do
+  dicionário. A função callbackFn tem dois parâmetros: key e value. Esse
+  método também pode ser interrompido caso a função de callback devolva
+  false (é semelhante ao método every da classe Array).
+ */
+
+function defaultToString(item) {
+  if (item === null) {
+    return 'NULL';
+  } if (item === undefined) {
+    return 'UNDEFINED';
+  } if (typeof item === 'string' || item instanceof String) {
+    return `${item}`;
+  }
+  return item.toString();
+}
+class ValuePair {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  toString() {
+    return `[#${this.key}: ${this.value}]`;
+  }
+}
+
+class Dictionary {
   constructor(toStrFn = defaultToString) {
-    this.toStrFn = toStrFn;
-    this.table = {};
-  }
-
-  set(key, value) {
-    if (key != null && value != null) {
-      const tableKey = this.toStrFn(key);
-      this.table[tableKey] = new ValuePair(key, value);
-      return true;
-    }
-    return false;
-  }
-
-  get(key) {
-    const valuePair = this.table[this.toStrFn(key)];
-    return valuePair == null ? undefined : valuePair.value;
+    this.toStrFn = toStrFn; // {1}
+    this.table = {}; // {2}
   }
 
   hasKey(key) {
     return this.table[this.toStrFn(key)] != null;
   }
 
-  remove(key) {
-    if (this.hasKey(key)) {
-      delete this.table[this.toStrFn(key)];
-      return true;
-    }
-    return false;
-  }
-
-  values() {
-    return this.keyValues().map(valuePair => valuePair.value);
-  }
-
-  keys() {
-    return this.keyValues().map(valuePair => valuePair.key);
-  }
-
-  keyValues() {
-    return Object.values(this.table);
-  }
-
-  forEach(callbackFn) {
-    const valuePairs = this.keyValues();
-    for (let i = 0; i < valuePairs.length; i++) {
-      const result = callbackFn(valuePairs[i].key, valuePairs[i].value);
-      if (result === false) {
-        break;
-      }
-    }
-  }
-
-  isEmpty() {
-    return this.size() === 0;
-  }
-
-  size() {
-    return Object.keys(this.table).length;
-  }
-
-  clear() {
-    this.table = {};
-  }
-
-  toString() {
-    if (this.isEmpty()) {
-      return '';
-    }
-    const valuePairs = this.keyValues();
-    let objString = `${valuePairs[0].toString()}`;
-    for (let i = 1; i < valuePairs.length; i++) {
-      objString = `${objString},${valuePairs[i].toString()}`;
-    }
-    return objString;
-  }
 }
+
+const dictionary = new Dictionary();
+
+console.log(dictionary);
