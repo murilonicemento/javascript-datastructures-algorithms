d0e0bf5e554a92aa480a1ddc2f696e8de84bb58d
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-20 22:16:15 -0300

    criação métodos getVertices e getAdjList

--- src/js/data-structures/graph.js
+++ src/js/data-structures/graph.js
@@ -158,8 +158,28 @@ class Graph {
       this.adjList.get(w).push(v); // {11}
     }
   }
+
+  getVertices() {
+    return this.vertices;
+  }
+
+  getAdjList() {
+    return this.adjList;
+  }
 }
 
 const graph = new Graph();
-
-console.log(graph);
+const myVertices = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']; // {12}
+for (let i = 0; i < myVertices.length; i++) { // {13}
+  graph.addVertex(myVertices[i]);
+}
+graph.addEdge('A', 'B'); // {14}
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('C', 'D');
+graph.addEdge('C', 'G');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
+graph.addEdge('E', 'I');
