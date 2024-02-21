5ef9c3e4075831af81a2c10e4bd6c4de9dd1b91c
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-21 20:07:52 -0300

    criação algoritmo BFS (Breadth First Search)

--- src/js/algorithms/graph/breadth-first-search.js
+++ src/js/algorithms/graph/breadth-first-search.js
@@ -1,4 +1,59 @@
-import Queue from '../../data-structures/queue';
+// import Queue from '../../data-structures/queue';
+
+class Queue {
+  constructor() {
+    this.count = 0;
+    this.lowestCount = 0;
+    this.items = {};
+  }
+
+  enqueue(element) {
+    this.items[this.count] = element;
+    this.count++;
+  }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount];
+    this.lowestCount++;
+    return result;
+  }
+
+  peek() {
+    if (this.isEmpty()) {
+      return undefined;
+    }
+    return this.items[this.lowestCount];
+  }
+
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+
+  size() {
+    return this.count - this.lowestCount;
+  }
+
+  toString() {
+    if (this.isEmpty()) {
+      return '';
+    }
+    let objString = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      objString = `${objString},${this.items[i]}`;
+    }
+    return objString;
+  }
+}
 
 const Colors = {
   WHITE: 0,
@@ -8,13 +63,15 @@ const Colors = {
 
 const initializeColor = vertices => {
   const color = {};
+
   for (let i = 0; i < vertices.length; i++) {
     color[vertices[i]] = Colors.WHITE;
   }
+
   return color;
 };
 
-export const breadthFirstSearch = (graph, startVertex, callback) => {
+const breadthFirstSearch = (graph, startVertex, callback) => {
   const vertices = graph.getVertices();
   const adjList = graph.getAdjList();
   const color = initializeColor(vertices);
@@ -26,49 +83,20 @@ export const breadthFirstSearch = (graph, startVertex, callback) => {
     const u = queue.dequeue();
     const neighbors = adjList.get(u);
     color[u] = Colors.GREY;
+
     for (let i = 0; i < neighbors.length; i++) {
       const w = neighbors[i];
+
       if (color[w] === Colors.WHITE) {
         color[w] = Colors.GREY;
         queue.enqueue(w);
       }
     }
+
     color[u] = Colors.BLACK;
+
     if (callback) {
       callback(u);
     }
   }
-};
-
-export const BFS = (graph, startVertex) => {
-  const vertices = graph.getVertices();
-  const adjList = graph.getAdjList();
-  const color = initializeColor(vertices);
-  const queue = new Queue();
-  const distances = {};
-  const predecessors = {};
-  queue.enqueue(startVertex);
-  for (let i = 0; i < vertices.length; i++) {
-    distances[vertices[i]] = 0;
-    predecessors[vertices[i]] = null;
-  }
-  while (!queue.isEmpty()) {
-    const u = queue.dequeue();
-    const neighbors = adjList.get(u);
-    color[u] = Colors.GREY;
-    for (let i = 0; i < neighbors.length; i++) {
-      const w = neighbors[i];
-      if (color[w] === Colors.WHITE) {
-        color[w] = Colors.GREY;
-        distances[w] = distances[u] + 1;
-        predecessors[w] = u;
-        queue.enqueue(w);
-      }
-    }
-    color[u] = Colors.BLACK;
-  }
-  return {
-    distances,
-    predecessors
-  };
 };
