172dc545dc69ac82961c9b71ee7c0ee2e7a17133
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-22 22:12:10 -0300

    criação algoritmos DFS

--- src/js/algorithms/graph/depth-first-search.js
+++ src/js/algorithms/graph/depth-first-search.js
@@ -15,72 +15,82 @@ const initializeColor = vertices => {
 };
 
 const depthFirstSearchVisit = (u, color, adjList, callback) => {
-  color[u] = Colors.GREY;
-  if (callback) {
+  color[u] = Colors.GREY; // {5}
+
+  if (callback) { // {6}
     callback(u);
   }
-  // console.log('Discovered ' + u);
-  const neighbors = adjList.get(u);
-  for (let i = 0; i < neighbors.length; i++) {
-    const w = neighbors[i];
-    if (color[w] === Colors.WHITE) {
-      depthFirstSearchVisit(w, color, adjList, callback);
+
+  const neighbors = adjList.get(u); // {7}
+
+  for (let i = 0; i < neighbors.length; i++) { // {8}
+    const w = neighbors[i]; // {9}
+
+    if (color[w] === Colors.WHITE) { // {10}
+      depthFirstSearchVisit(w, color, adjList, callback); // {11}
     }
   }
-  color[u] = Colors.BLACK;
-  // console.log('explored ' + u);
+
+  color[u] = Colors.BLACK; // {12}
 };
 
-export const depthFirstSearch = (graph, callback) => {
+const depthFirstSearch = (graph, callback) => { // {1}
   const vertices = graph.getVertices();
   const adjList = graph.getAdjList();
   const color = initializeColor(vertices);
 
-  for (let i = 0; i < vertices.length; i++) {
-    if (color[vertices[i]] === Colors.WHITE) {
-      depthFirstSearchVisit(vertices[i], color, adjList, callback);
+  for (let i = 0; i < vertices.length; i++) { // {2}
+    if (color[vertices[i]] === Colors.WHITE) { // {3}
+      depthFirstSearchVisit(vertices[i], color, adjList, callback); // {4}
     }
   }
 };
 
 const DFSVisit = (u, color, d, f, p, time, adjList) => {
-  // console.log('discovered ' + u);
   color[u] = Colors.GREY;
-  d[u] = ++time.count;
+  d[u] = ++time.count; // {4}
   const neighbors = adjList.get(u);
+
   for (let i = 0; i < neighbors.length; i++) {
     const w = neighbors[i];
+
     if (color[w] === Colors.WHITE) {
-      p[w] = u;
+      p[w] = u; // {5}
       DFSVisit(w, color, d, f, p, time, adjList);
     }
   }
+
   color[u] = Colors.BLACK;
-  f[u] = ++time.count;
-  // console.log('explored ' + u);
+  f[u] = ++time.count; // {6}
+
 };
 
-export const DFS = graph => {
+const DFS = graph => {
   const vertices = graph.getVertices();
   const adjList = graph.getAdjList();
   const color = initializeColor(vertices);
   const d = {};
   const f = {};
   const p = {};
-  const time = { count: 0 };
-  for (let i = 0; i < vertices.length; i++) {
+  const time = { count: 0 }; // {1}
+
+  for (let i = 0; i < vertices.length; i++) { // {2}
     f[vertices[i]] = 0;
     d[vertices[i]] = 0;
     p[vertices[i]] = null;
   }
+
   for (let i = 0; i < vertices.length; i++) {
     if (color[vertices[i]] === Colors.WHITE) {
       DFSVisit(vertices[i], color, d, f, p, time, adjList);
     }
   }
-  return {
+
+  return { // {3}
     discovery: d,
     finished: f,
     predecessors: p
   };
 };
+
+export { depthFirstSearch };
