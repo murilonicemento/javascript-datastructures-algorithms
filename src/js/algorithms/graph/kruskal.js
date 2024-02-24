598ac9729f09685b12923ed00a79b9cb180c1bd6
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-24 15:17:16 -0300

    Criação algoritmo Kruskal

--- src/js/algorithms/graph/kruskal.js
+++ src/js/algorithms/graph/kruskal.js
@@ -1,20 +1,26 @@
 const INF = Number.MAX_SAFE_INTEGER;
+
 const find = (i, parent) => {
   while (parent[i]) {
-    i = parent[i]; // eslint-disable-line prefer-destructuring
+    i = parent[i];
   }
+
   return i;
 };
+
 const union = (i, j, parent) => {
   if (i !== j) {
     parent[j] = i;
     return true;
   }
+
   return false;
 };
+
 const initializeCost = graph => {
   const cost = [];
   const { length } = graph;
+
   for (let i = 0; i < length; i++) {
     cost[i] = [];
     for (let j = 0; j < length; j++) {
@@ -25,19 +31,19 @@ const initializeCost = graph => {
       }
     }
   }
+
   return cost;
 };
-export const kruskal = graph => {
+
+const kruskal = graph => {
   const { length } = graph;
   const parent = [];
   let ne = 0;
-  let a;
-  let b;
-  let u;
-  let v;
-  const cost = initializeCost(graph);
-  while (ne < length - 1) {
-    for (let i = 0, min = INF; i < length; i++) {
+  let a; let b; let u; let v;
+  const cost = initializeCost(graph); // {1}
+
+  while (ne < length - 1) { // {2}
+    for (let i = 0, min = INF; i < length; i++) { // {3}
       for (let j = 0; j < length; j++) {
         if (cost[i][j] < min) {
           min = cost[i][j];
@@ -46,12 +52,15 @@ export const kruskal = graph => {
         }
       }
     }
-    u = find(u, parent);
-    v = find(v, parent);
-    if (union(u, v, parent)) {
-      ne++;
-    }
-    cost[a][b] = cost[b][a] = INF;
+    u = find(u, parent); // {4}
+    v = find(v, parent); // {5}
+
+    if (union(u, v, parent)) ne++; // {6}
+
+    cost[a][b] = cost[b][a] = INF; // {7}
   }
+
   return parent;
 };
+
+export { kruskal };
