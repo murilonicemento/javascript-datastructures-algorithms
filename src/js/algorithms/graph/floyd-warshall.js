e922e1c2c49a8969f2329db92e1ed5a3c96f5645
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-24 12:38:51 -0300

    Criação algoritmo floyd warshall

--- src/js/algorithms/graph/floyd-warshall.js
+++ src/js/algorithms/graph/floyd-warshall.js
@@ -1,26 +1,32 @@
-export const floydWarshall = graph => {
+const floydWarshall = graph => {
   const dist = [];
   const { length } = graph;
-  for (let i = 0; i < length; i++) {
+
+  for (let i = 0; i < length; i++) { // {1}
     dist[i] = [];
+
     for (let j = 0; j < length; j++) {
       if (i === j) {
-        dist[i][j] = 0;
+        dist[i][j] = 0; // {2}
       } else if (!isFinite(graph[i][j])) {
-        dist[i][j] = Infinity;
+        dist[i][j] = Infinity; // {3}
       } else {
-        dist[i][j] = graph[i][j];
+        dist[i][j] = graph[i][j]; // {4}
       }
     }
   }
-  for (let k = 0; k < length; k++) {
+
+  for (let k = 0; k < length; k++) { // {5}
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length; j++) {
-        if (dist[i][k] + dist[k][j] < dist[i][j]) {
-          dist[i][j] = dist[i][k] + dist[k][j];
+        if (dist[i][k] + dist[k][j] < dist[i][j]) { // {6}
+          dist[i][j] = dist[i][k] + dist[k][j]; // {7}
         }
       }
     }
   }
+
   return dist;
 };
+
+export { floydWarshall };
