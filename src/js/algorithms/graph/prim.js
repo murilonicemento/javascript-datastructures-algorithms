36ffda4e0c0420a69529d20970ce5da97d04ee70
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-24 13:09:17 -0300

    Criação algoritmo Prim

--- src/js/algorithms/graph/prim.js
+++ src/js/algorithms/graph/prim.js
@@ -1,4 +1,5 @@
 const INF = Number.MAX_SAFE_INTEGER;
+
 const minKey = (graph, key, visited) => {
   // Initialize min value
   let min = INF;
@@ -11,26 +12,34 @@ const minKey = (graph, key, visited) => {
   }
   return minIndex;
 };
-export const prim = graph => {
+
+const prim = graph => {
   const parent = [];
   const key = [];
   const visited = [];
   const { length } = graph;
-  for (let i = 0; i < length; i++) {
+
+  for (let i = 0; i < length; i++) { // {1}
     key[i] = INF;
     visited[i] = false;
   }
-  key[0] = 0;
+
+  key[0] = 0; // {2}
   parent[0] = -1;
-  for (let i = 0; i < length - 1; i++) {
-    const u = minKey(graph, key, visited);
-    visited[u] = true;
+
+  for (let i = 0; i < length; i++) { // {3}
+    const u = minKey(graph, key, visited); // {4}
+    visited[u] = true; // {5}
+
     for (let v = 0; v < length; v++) {
-      if (graph[u][v] && !visited[v] && graph[u][v] < key[v]) {
-        parent[v] = u;
-        key[v] = graph[u][v];
+      if (graph[u][v] && !visited[v] && graph[u][v] < key[v]) { // {6}
+        parent[v] = u; // {7}
+        key[v] = graph[u][v]; // {8}
       }
     }
   }
-  return parent;
+
+  return parent; // {9}
 };
+
+export { prim };
