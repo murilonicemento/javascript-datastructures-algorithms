5eaf0e92be4f84a3946fcc641e940b470d283b88
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-24 12:19:26 -0300

    Criação algoritmo dijkstra

--- src/js/algorithms/graph/dijkstra.js
+++ src/js/algorithms/graph/dijkstra.js
@@ -1,32 +1,47 @@
 const INF = Number.MAX_SAFE_INTEGER;
+
 const minDistance = (dist, visited) => {
   let min = INF;
   let minIndex = -1;
+
   for (let v = 0; v < dist.length; v++) {
     if (visited[v] === false && dist[v] <= min) {
       min = dist[v];
       minIndex = v;
     }
   }
+
   return minIndex;
 };
-export const dijkstra = (graph, src) => {
+
+const dijkstra = (graph, src) => {
   const dist = [];
   const visited = [];
   const { length } = graph;
-  for (let i = 0; i < length; i++) {
+
+  for (let i = 0; i < length; i++) { // {1}
     dist[i] = INF;
     visited[i] = false;
   }
-  dist[src] = 0;
-  for (let i = 0; i < length - 1; i++) {
-    const u = minDistance(dist, visited);
-    visited[u] = true;
+
+  dist[src] = 0; // {2}
+
+  for (let i = 0; i < length - 1; i++) { // {3}
+    const u = minDistance(dist, visited); // {4}
+    visited[u] = true; // {5}
+
     for (let v = 0; v < length; v++) {
-      if (!visited[v] && graph[u][v] !== 0 && dist[u] !== INF && dist[u] + graph[u][v] < dist[v]) {
-        dist[v] = dist[u] + graph[u][v];
+      if (!visited[v]
+        && graph[u][v] !== 0
+        && dist[u] !== INF
+        && dist[u] + graph[u][v] < dist[v]
+      ) { // {6}
+        dist[v] = dist[u] + graph[u][v]; // {7}
       }
     }
   }
-  return dist;
+
+  return dist; // {8}
 };
+
+export { dijkstra };
