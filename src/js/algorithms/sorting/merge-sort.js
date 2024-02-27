f604bf23f1991fdeb7d140e586f0c1b9b054f6b6
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-27 20:03:48 -0300

    criação algoritmo merge sort

--- src/js/algorithms/sorting/merge-sort.js
+++ src/js/algorithms/sorting/merge-sort.js
@@ -1,21 +1,40 @@
-import { Compare, defaultCompare } from '../../util';
+// import { Compare, defaultCompare } from '../../util';
+
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
+};
+
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
 
 function merge(left, right, compareFn) {
-  let i = 0;
+  let i = 0; // {6}
   let j = 0;
   const result = [];
-  while (i < left.length && j < right.length) {
-    result.push(compareFn(left[i], right[j]) === Compare.LESS_THAN ? left[i++] : right[j++]);
+
+  while (i < left.length && j < right.length) { // {7}
+    result.push(compareFn(left[i], right[j]) === Compare.LESS_THAN ? left[i++] : right[j++]); // {8}
   }
-  return result.concat(i < left.length ? left.slice(i) : right.slice(j));
+
+  return result.concat(i < left.length ? left.slice(i) : right.slice(j)); // {9}
 }
-export function mergeSort(array, compareFn = defaultCompare) {
-  if (array.length > 1) {
+
+function mergeSort(array, compareFn = defaultCompare) {
+  if (array.length > 1) { // {1}
     const { length } = array;
-    const middle = Math.floor(length / 2);
-    const left = mergeSort(array.slice(0, middle), compareFn);
-    const right = mergeSort(array.slice(middle, length), compareFn);
-    array = merge(left, right, compareFn);
+    const middle = Math.floor(length / 2); // {2}
+    const left = mergeSort(array.slice(0, middle), compareFn); // {3}
+    const right = mergeSort(array.slice(middle, length), compareFn); // {4}
+    array = merge(left, right, defaultCompare); // {4}
   }
+
   return array;
 }
+
+export { mergeSort };
