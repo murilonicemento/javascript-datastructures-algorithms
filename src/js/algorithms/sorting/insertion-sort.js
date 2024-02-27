261b6452d48af9da12fa0a968ddc4cad0fb98831
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-26 22:30:43 -0300

    criação do algoritmos insertion sort

--- src/js/algorithms/sorting/insertion-sort.js
+++ src/js/algorithms/sorting/insertion-sort.js
@@ -1,19 +1,46 @@
-import { Compare, defaultCompare } from '../../util';
+// import { Compare, defaultCompare } from '../../util';
 
-export const insertionSort = (array, compareFn = defaultCompare) => {
-  const { length } = array;
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
+};
+
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+function insertionSort(array, compareFn = defaultCompare) {
+  const { length } = array; // {1}
   let temp;
-  for (let i = 1; i < length; i++) {
-    let j = i;
-    temp = array[i];
-    // console.log('to be inserted ' + temp);
-    while (j > 0 && compareFn(array[j - 1], temp) === Compare.BIGGER_THAN) {
-      // console.log('shift ' + array[j - 1]);
-      array[j] = array[j - 1];
+
+  for (let i = 1; i < length; i++) { // {2}
+    let j = i; // {3}
+    temp = array[i]; // {4}
+
+    while (j > 0 && compareFn(array[j - 1], temp) === Compare.BIGGER_THAN) { // {5}
+      array[j] = array[j - 1]; // {6}
       j--;
     }
-    // console.log('insert ' + temp);
-    array[j] = temp;
+
+    array[j] = temp; // {7}
+  }
+
+  return array;
+}
+
+function createNonSortedArray(size) { // 6
+  const array = [];
+  for (let i = size; i > 0; i--) {
+    array.push(i);
   }
   return array;
-};
+}
+
+let array = createNonSortedArray(5); // {7}
+console.log(array.join()); // {8}
+array = insertionSort(array); // {9}
+console.log(array.join()); // {10}
