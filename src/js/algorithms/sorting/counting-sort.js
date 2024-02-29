838965d6c8066e436382d4165538394c7af898e4
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-28 21:44:51 -0300

    criação do algoritmo counting sort

--- src/js/algorithms/sorting/counting-sort.js
+++ src/js/algorithms/sorting/counting-sort.js
@@ -1,24 +1,57 @@
-import { findMaxValue } from '../search/min-max-search';
+// import { findMaxValue } from '../search/min-max-search';
 
-export function countingSort(array) {
-  if (array.length < 2) {
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
+};
+
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+function findMaxValue(array, compareFn = defaultCompare) {
+  if (array && array.length > 0) {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (compareFn(max, array[i]) === Compare.LESS_THAN) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
+  return undefined;
+}
+
+function countingSort(array) {
+  if (array.length < 2) { // {1}
     return array;
   }
-  const maxValue = findMaxValue(array);
-  let sortedIndex = 0;
-  const counts = new Array(maxValue + 1);
+
+  const maxValue = findMaxValue(array); // {2}
+  const counts = new Array(maxValue + 1); // {3}
+
   array.forEach(element => {
-    if (!counts[element]) {
+    if (!counts[element]) { // {4}
       counts[element] = 0;
     }
-    counts[element]++;
+
+    counts[element]++; // {5}
   });
-  // console.log('Frequencies: ' + counts.join());
-  counts.forEach((element, i) => {
-    while (element > 0) {
-      array[sortedIndex++] = i;
-      element--;
+
+  let sortedIndex = 0;
+
+  counts.forEach((count, i) => {
+    while (count > 0) { // {6}
+      array[sortedIndex++] = i; // {7}
+      count--; // {8}
     }
   });
+
   return array;
 }
+
+export { countingSort };
