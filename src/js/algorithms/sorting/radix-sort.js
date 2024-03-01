b05ef73f4b778d39016aab2dea9490e2efad3209
murilonicemento <murilo_nascimento22@outlook.com>
2024-02-29 21:56:54 -0300

    criação algoritmo radix sort

--- src/js/algorithms/sorting/radix-sort.js
+++ src/js/algorithms/sorting/radix-sort.js
@@ -1,44 +1,87 @@
-import { findMaxValue, findMinValue } from '../search/min-max-search';
+// import { findMaxValue, findMinValue } from '../search/min-max-search';
 
-const getBucketIndex = (value, minValue, significantDigit, radixBase) =>
-  Math.floor(((value - minValue) / significantDigit) % radixBase);
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0
+};
 
-const countingSortForRadix = (array, radixBase, significantDigit, minValue) => {
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+function findMaxValue(array, compareFn = defaultCompare) {
+  if (array && array.length > 0) {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (compareFn(max, array[i]) === Compare.LESS_THAN) {
+        max = array[i];
+      }
+    }
+    return max;
+  }
+  return undefined;
+}
+
+function findMinValue(array, compareFn = defaultCompare) {
+  if (array && array.length > 0) {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (compareFn(min, array[i]) === Compare.BIGGER_THAN) {
+        min = array[i];
+      }
+    }
+    return min;
+  }
+  return undefined;
+}
+
+function countingSortForRadix(array, radixBase, significantDigit, minValue) {
   let bucketsIndex;
   const buckets = [];
   const aux = [];
-  for (let i = 0; i < radixBase; i++) {
+
+  for (let i = 0; i < radixBase; i++) { // {5}
     buckets[i] = 0;
   }
-  for (let i = 0; i < array.length; i++) {
-    bucketsIndex = getBucketIndex(array[i], minValue, significantDigit, radixBase);
-    buckets[bucketsIndex]++;
+
+  for (let i = 0; i < array.length; i++) { // {6}
+    bucketsIndex = Math.floor(((array[i] - minValue) / significantDigit) % radixBase); // {7}
+    buckets[bucketsIndex]++; // {8}
   }
-  for (let i = 1; i < radixBase; i++) {
+
+  for (let i = 1; i < radixBase; i++) { // {9}
     buckets[i] += buckets[i - 1];
   }
-  for (let i = array.length - 1; i >= 0; i--) {
-    bucketsIndex = getBucketIndex(array[i], minValue, significantDigit, radixBase);
-    aux[--buckets[bucketsIndex]] = array[i];
+
+  for (let i = array.length - 1; i >= 0; i--) { // {10}
+    bucketsIndex = Math.floor(((array[i] - minValue) / significantDigit) % radixBase); // {11}
+    aux[--buckets[bucketsIndex]] = array[i]; // {12}
   }
-  for (let i = 0; i < array.length; i++) {
+
+  for (let i = 0; i < array.length; i++) { // {13}
     array[i] = aux[i];
   }
-  return array;
-};
-export function radixSort(array, radixBase = 10) {
-  if (array.length < 2) {
-    return array;
-  }
-  const minValue = findMinValue(array);
-  const maxValue = findMaxValue(array);
-  // Perform counting sort for each significant digit, starting at 1
-  let significantDigit = 1;
-  while ((maxValue - minValue) / significantDigit >= 1) {
-    // console.log('radix sort for digit ' + significantDigit);
-    array = countingSortForRadix(array, radixBase, significantDigit, minValue);
-    // console.log(array.join());
-    significantDigit *= radixBase;
-  }
+
   return array;
 }
+
+function radixSort(array, radixBase = 10) {
+  if (array.length < 2) return array;
+
+  const minValue = findMinValue(array);
+  const maxValue = findMaxValue(array);
+  let significantDigit = 1; // {1}
+
+  while ((maxValue - minValue) / significantDigit >= 1) { // {2}
+    array = countingSortForRadix(array, radixBase, significantDigit, minValue); // {3}
+    significantDigit *= radixBase; // {4}
+  }
+
+  return array;
+}
+
+export { radixSort };
