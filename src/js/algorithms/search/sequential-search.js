dbc1305fd887b68b8c55381c81dfc84bd60838c6
murilonicemento <murilo_nascimento22@outlook.com>
2024-03-02 10:43:51 -0300

    criação algoritmo sequential search

--- src/js/algorithms/search/sequential-search.js
+++ src/js/algorithms/search/sequential-search.js
@@ -1,10 +1,19 @@
-import { defaultEquals, DOES_NOT_EXIST } from '../../util';
+// import { defaultEquals, DOES_NOT_EXIST } from '../../util';
 
-export function sequentialSearch(array, value, equalsFn = defaultEquals) {
-  for (let i = 0; i < array.length; i++) {
-    if (equalsFn(value, array[i])) {
-      return i;
+function defaultEquals(a, b) {
+  return a === b;
+}
+
+const DOES_NOT_EXIST = -1;
+
+function sequentialSearch(array, value, equalsFn = defaultEquals) {
+  for (let i = 0; i < array.length; i++) { // {1}
+    if (equalsFn(value, array[i])) { // {2}
+      return i; // {3}
     }
   }
-  return DOES_NOT_EXIST;
+
+  return DOES_NOT_EXIST; // {4}
 }
+
+export { sequentialSearch };
