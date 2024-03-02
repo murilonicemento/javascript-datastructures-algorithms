ceae4ef6cd1483f8e2d19c3e75c1c9b89f489118
murilonicemento <murilo_nascimento22@outlook.com>
2024-03-02 12:34:42 -0300

    criação algoritmo shuffle

--- src/js/algorithms/shuffle/fisher–yates.js
+++ src/js/algorithms/shuffle/fisher–yates.js
@@ -1,11 +1,19 @@
-import { swap } from '../../util';
+// import { swap } from '../../util';
 
-export function shuffle(array) {
-  let currentIndex = array.length;
-  while (currentIndex !== 0) {
-    const randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-    swap(array, currentIndex, randomIndex);
+function swap(array, a, b) {
+  /* const temp = array[a];
+  array[a] = array[b];
+  array[b] = temp; */
+  [array[a], array[b]] = [array[b], array[a]];
+}
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * (i + 1));
+    swap(array, i, randomIndex);
   }
+
   return array;
 }
+
+export { shuffle };
