210854e639c7ee187d3feb6381809d1399ff8286
murilonicemento <murilo_nascimento22@outlook.com>
2024-03-09 11:48:35 -0300

    criação algoritmo matrix chain multiplication

--- src/js/algorithms/dynamic-programing/matrix-chain-multiplication.js
+++ src/js/algorithms/dynamic-programing/matrix-chain-multiplication.js
@@ -9,37 +9,45 @@ function printOptimalParenthesis(s, i, j) {
   }
 }
 
-export function matrixChainOrder(p) {
+function matrixChainOrder(p) {
   const n = p.length;
   const m = [];
   const s = [];
+
   for (let i = 1; i <= n; i++) {
     m[i] = [];
     m[i][i] = 0;
   }
+
   for (let i = 0; i <= n; i++) {
-    // to help printing the optimal solution
-    s[i] = []; // auxiliary
+    s[i] = [];
+
     for (let j = 0; j <= n; j++) {
       s[i][j] = 0;
     }
   }
+
   for (let l = 2; l < n; l++) {
-    for (let i = 1; i <= (n - l) + 1; i++) {
+    for (let i = 1; i <= n; i++) {
       const j = (i + l) - 1;
       m[i][j] = Number.MAX_SAFE_INTEGER;
+
       for (let k = i; k <= j - 1; k++) {
-        // q = cost/scalar multiplications
-        const q = m[i][k] + m[k + 1][j] + ((p[i - 1] * p[k]) * p[j]);
+        const q = m[i][k] + m[k + 1][j] + ((p[i - 1] * p[k] * p[j])); // {1}
+
         if (q < m[i][j]) {
-          m[i][j] = q;
-          s[i][j] = k; // s[i,j] = Second auxiliary table that stores k
+          m[i][j] = q; // {2}
+          s[i][j] = k;
         }
       }
     }
   }
-  // console.log(m);
-  // console.log(s);
+
   printOptimalParenthesis(s, 1, n - 1);
-  return m[1][n - 1];
+  return m[1][n - 1]; // {3}
 }
+
+const p = [10, 100, 5, 50, 1];
+console.log(matrixChainOrder(p));
+
+export { matrixChainOrder };
